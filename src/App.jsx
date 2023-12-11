3d3e80590ad3fa369d172a20cbe7fd5c6298db11
Rezoan Ahmed <rezoan.cse.2017@gmail.com>
2023-12-11 19:10:40 +0600

    created footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Shared/Navbar/Navbar";
+import Footer from "./Components/Footer/Footer";
 
 const App = () => {
   return (
     <div>
       <Navbar></Navbar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+
+const Footer = () => {
+    return (
+        <div className="mt-10">
+            <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+                <aside>
+                    <p>Copyright © 2023 - All right reserved by Md Rezoan Ahmed</p>
+                </aside>
+            </footer>
+        </div>
+    );
+};
+
+export default Footer;
